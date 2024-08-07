63fda6eba8adc6e54dbeacdd030a95390cfc6b74
Loanda Gunawan <loanda.gunawan@ui.ac.id>
2024-08-08 01:45:28 +0700

    Define routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import AppLayout from "./layouts/AppLayout";
+
+import RedirectLink from "./pages/RedirectLink";
+import LandingPage from "./pages/LandingPage";
+import Dashboard from "./pages/Dashboard";
+import LinkPage from "./pages/LinkPage";
+import Auth from "./pages/Auth";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/auth",
+        element: <Auth />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/link/:id",
+        element: <LinkPage />,
+      },
+      {
+        path: "/:id",
+        element: <RedirectLink />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/layouts/AppLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <main className="min-h-screen container">
+        {/* <Header /> */}
+        <Outlet />
+      </main>
+      {/* <Footer /> */}
+    </div>
+  );
+};
+
+export default AppLayout;
