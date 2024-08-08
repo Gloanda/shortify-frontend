808370c1963e382391023385c2b2212b8ff08121
Loanda Gunawan <loanda.gunawan@ui.ac.id>
2024-08-08 18:23:52 +0700

    Redirect authenticated user from auth to dashboard

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,11 +1,21 @@
 import Login from "@/components/login";
 import Signup from "@/components/signup";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { useSearchParams } from "react-router-dom";
+import { UrlState } from "@/Context";
+import { useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 function Auth() {
   let [searchParams] = useSearchParams();
-  
+  const navigate = useNavigate();
+  const { isAuthenticated, loading } = UrlState();
+  const longLink = searchParams.get("createNew");
+
+  useEffect(() => {
+    if (isAuthenticated && !loading)
+      navigate(`/dashboard?${longLink ? `createNew=${longLink}` : ""}`);
+  }, [isAuthenticated, loading, navigate, longLink]);
+
   return (
     <div className="mt-36 flex flex-col items-center gap-10">
       <h1 className="text-5xl font-extrabold">
