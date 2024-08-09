a153a1c01773991871ba18481dd0b48e7571f3b7
Loanda Gunawan <loanda.gunawan@ui.ac.id>
2024-08-09 11:17:00 +0700

    Create API for urls and clicks

--- src/utils/apiAuth.js
+++ src/utils/apiAuth.js
@@ -1,6 +1,4 @@
-import supabase from "./supabase";
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+import supabase, {supabaseUrl} from "./supabase";
 
 export async function login({ email, password }) {
   const { data, error } = await supabase.auth.signInWithPassword({ email, password });
